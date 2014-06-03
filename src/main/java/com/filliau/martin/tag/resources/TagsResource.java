894c36efade06b61e788791d3413c05de72feb0f
Martin Filliau <martin@filliau.com>
2014-06-03 16:38:55 +0100

    GET list of tags matching name query

--- src/main/java/com/filliau/martin/tag/resources/TagsResource.java
+++ src/main/java/com/filliau/martin/tag/resources/TagsResource.java
@@ -1,13 +1,23 @@
 package com.filliau.martin.tag.resources;
 
 import com.filliau.martin.tag.representations.Tag;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
+import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
 
 /**
  *
@@ -25,7 +35,24 @@ public class TagsResource {
     
     @GET
     public List<Tag> getTag(@QueryParam("name") String name) {
-        return null;
+        SolrQuery q = new SolrQuery();
+        q.setQuery("name:"+name);
+        QueryResponse rsp;
+        try {
+            rsp = solr.query(q);
+            SolrDocumentList docs = rsp.getResults();
+            List<Tag> tags = new ArrayList<Tag>();
+            Iterator<SolrDocument> iter = rsp.getResults().iterator();
+            SolrDocument resultDoc;
+            while (iter.hasNext()) {
+                resultDoc = iter.next();
+                tags.add(Tag.fromSolrDocument(resultDoc));
+            }
+            return tags;
+        } catch (SolrServerException ex) {
+            Logger.getLogger(TagsResource.class.getName()).log(Level.SEVERE, null, ex);
+            throw new WebApplicationException(500);
+        }
     }
 
 }
